6395efa82c5562108f408930dfc7ba5ce8374b5a
Pau Julió Plana <pau.julio.plana@gmail.com>
2019-10-05 16:07:55 +0200

    Initial commit + Hello World

---
+++ app/src/main/java/example/pau/rickmorty/CharacterSelector.java
@@ -0,0 +1,13 @@
+package example.pau.rickmorty;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CharacterSelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_character_selector);
+    }
+}
