39fa70d5063a3f1dbfe0760ff069eba109c4062b
Pau Julió Plana <pau.julio.plana@gmail.com>
2019-10-05 16:24:43 +0200

    Character class created

---
+++ app/src/main/java/example/pau/rickmorty/Character.java
@@ -0,0 +1,88 @@
+package example.pau.rickmorty;
+
+import java.lang.reflect.Array;
+
+public class Character {
+
+    private int chId;
+    private String chName;
+    private String chStatus;;
+    private String chSpecies;
+    private String chType;
+    private String chGender;
+    private String chOrigin;
+    private String chLocation;
+    private String chImage;
+
+    public int getChId() {
+        return chId;
+    }
+
+    public void setChId(int chId) {
+        this.chId = chId;
+    }
+
+    public String getChName() {
+        return chName;
+    }
+
+    public void setChName(String chName) {
+        this.chName = chName;
+    }
+
+    public String getChStatus() {
+        return chStatus;
+    }
+
+    public void setChStatus(String chStatus) {
+        this.chStatus = chStatus;
+    }
+
+    public String getChSpecies() {
+        return chSpecies;
+    }
+
+    public void setChSpecies(String chSpecies) {
+        this.chSpecies = chSpecies;
+    }
+
+    public String getChType() {
+        return chType;
+    }
+
+    public void setChType(String chType) {
+        this.chType = chType;
+    }
+
+    public String getChGender() {
+        return chGender;
+    }
+
+    public void setChGender(String chGender) {
+        this.chGender = chGender;
+    }
+
+    public String getChOrigin() {
+        return chOrigin;
+    }
+
+    public void setChOrigin(String chOrigin) {
+        this.chOrigin = chOrigin;
+    }
+
+    public String getChLocation() {
+        return chLocation;
+    }
+
+    public void setChLocation(String chLocation) {
+        this.chLocation = chLocation;
+    }
+
+    public String getChImage() {
+        return chImage;
+    }
+
+    public void setChImage(String chImage) {
+        this.chImage = chImage;
+    }
+}
