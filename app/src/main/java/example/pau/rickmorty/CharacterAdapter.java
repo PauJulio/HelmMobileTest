f1be35c17b9018db033ebb348b7d0149408af664
Pau Julió Plana <pau.julio.plana@gmail.com>
2019-10-07 05:52:51 +0200

    Navigating to the next activity successfully

--- app/src/main/java/example/pau/rickmorty/CharacterAdapter.java
+++ app/src/main/java/example/pau/rickmorty/CharacterAdapter.java
@@ -1,6 +1,7 @@
 package example.pau.rickmorty;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -18,31 +19,27 @@ public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.View
 
     private Context chContext;
     private ArrayList<Character> characters;
+    private ClickListener clickListener;
 
-    public CharacterAdapter(Context chContext, ArrayList<Character> characters) {
+    public CharacterAdapter(Context chContext, ArrayList<Character> characters, ClickListener clickListener) {
         this.chContext = chContext;
         this.characters = new ArrayList<>(characters);
+        this.clickListener = clickListener;
     }
 
 
     @Override
     public CharacterAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
         View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.character, viewGroup, false);
-        ViewHolder viewHolder = new ViewHolder(view);
+        ViewHolder viewHolder = new ViewHolder(view, clickListener);
 
         return viewHolder;
     }
 
     @Override
-    public void onBindViewHolder(@NonNull CharacterAdapter.ViewHolder viewHolder, int position) {
+    public void onBindViewHolder(@NonNull CharacterAdapter.ViewHolder viewHolder, final int position) {
         Glide.with(chContext).asBitmap().load(characters.get(position).getChImage()).into(viewHolder.chImage);
         viewHolder.chName.setText(characters.get(position).getChName());
-        viewHolder.parentLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
 
     }
 
@@ -51,18 +48,30 @@ public class CharacterAdapter extends RecyclerView.Adapter<CharacterAdapter.View
         return characters.size();
     }
 
-    public class ViewHolder extends RecyclerView.ViewHolder{
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
 
         ImageView chImage;
         TextView chName;
         RelativeLayout parentLayout;
+        ClickListener clickListener;
 
-        public ViewHolder(@NonNull View itemView) {
+        public ViewHolder(@NonNull View itemView, ClickListener clickListener) {
             super(itemView);
             chImage = itemView.findViewById(R.id.chImage);
             chName = itemView.findViewById(R.id.chName);
             parentLayout = itemView.findViewById(R.id.parent_layout);
+            this.clickListener = clickListener;
+            itemView.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            clickListener.onClick(getAdapterPosition());
+        }
+    }
+
+    public interface ClickListener{
+        void onClick (int position);
     }
 
 
--- app/src/main/java/example/pau/rickmorty/CharacterSelector.java
+++ app/src/main/java/example/pau/rickmorty/CharacterSelector.java
@@ -1,5 +1,6 @@
 package example.pau.rickmorty;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -16,10 +17,11 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class CharacterSelector extends AppCompatActivity {
+public class CharacterSelector extends AppCompatActivity implements CharacterAdapter.ClickListener {
 
     private CharacterAdapter characterAdapter;
     private ArrayList<Character> characters = new ArrayList<>();
+    private ArrayList<Boolean> likes;
     private static final String BASE_URL = "https://rickandmortyapi.com/api/";
 
 
@@ -33,7 +35,7 @@ public class CharacterSelector extends AppCompatActivity {
 
     private void recycleInit(){
         RecyclerView recycleView = findViewById(R.id.recycleView);
-        characterAdapter = new CharacterAdapter(this, characters);
+        characterAdapter = new CharacterAdapter(this, characters, this);
         recycleView.setAdapter(characterAdapter);
         recycleView.setLayoutManager(new LinearLayoutManager(this));
     }
@@ -50,6 +52,7 @@ public class CharacterSelector extends AppCompatActivity {
             @Override
             public void onResponse(Call<ResultsFromAPI> call, Response<ResultsFromAPI> response) {
                 characters = response.body().getCharacters();
+                likes = new ArrayList<>(characters.size());
                 recycleInit();
             }
 
@@ -60,5 +63,16 @@ public class CharacterSelector extends AppCompatActivity {
         });
     }
 
-
+    @Override
+    public void onClick(int position) {
+        Intent intent = new Intent(this, CharacterSpecificActivity.class);
+        intent.putExtra("chName", characters.get(position).getChName());
+        intent.putExtra("chStatus", characters.get(position).getChStatus());
+        intent.putExtra("chSpecies", characters.get(position).getChSpecies());
+        intent.putExtra("chType", characters.get(position).getChType());
+        intent.putExtra("chGender", characters.get(position).getChGender());
+        intent.putExtra("chImage", characters.get(position).getChImage());
+        //intent.putExtra("like", likes.get(position));
+        startActivity(intent);
+    }
 }
